9ebdfcd3b0666f969beac5c8da21b1595d618bed
Immanuel Buder <ibuder@gmail.com>
2015-05-19 15:58:10 -0700

    Add data sources, initial munging script

---
+++ munge_school_sat.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 12:22:07 2015
+
+@author: ibuder
+"""
+
+import pandas as pd
+#  sat1314.xls has data on the second sheet (first is a title page)
+#  first column is the school code (CDS)
+sat = pd.io.excel.read_excel('sat1314.xls', sheetname=1, index_col=0)
+
+#  pubschls.xls first column is the school code (CDS)
+schools = pd.io.excel.read_excel('pubschls.xls', index_col=0)
+
+sat_schools = pd.merge(sat[['RTYPE', 'NUMTSTTAKR', 'AVGSCRREAD',
+                            'AVGSCRMATH', 'AVGSCRWRIT']],
+                            schools[['School', 'Street', 'City',
+                            'Zip', 'State', 'Latitude', 'Longitude']],
+                            how='inner',
+                            left_index=True, right_index=True)
