39cd536f44baca5251eff0570f793cb22517b2b0
Immanuel Buder <ibuder@gmail.com>
2015-05-19 20:06:44 -0700

    add .gitignore (ignore files with copyright issues) make munge_school_sat.py output json

--- munge_school_sat.py
+++ munge_school_sat.py
@@ -6,6 +6,8 @@ Created on Tue May 19 12:22:07 2015
 """
 
 import pandas as pd
+
+
 #  sat1314.xls has data on the second sheet (first is a title page)
 #  first column is the school code (CDS)
 sat = pd.io.excel.read_excel('sat1314.xls', sheetname=1, index_col=0)
@@ -19,3 +21,6 @@ sat_schools = pd.merge(sat[['RTYPE', 'NUMTSTTAKR', 'AVGSCRREAD',
                             'Zip', 'State', 'Latitude', 'Longitude']],
                             how='inner',
                             left_index=True, right_index=True)
+
+#FIXME some schools do not have Lat/Long.
+sat_schools.to_json('sat_schools.json', orient='records')
