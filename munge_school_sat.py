ba001d89beb8e4462d29d3f27a1f52a74e4a5147
Immanuel Buder <ibuder@gmail.com>
2015-05-21 15:18:44 -0700

    Tooltip displays school SAT score drop districts and counties in munge_school_sat.py

--- munge_school_sat.py
+++ munge_school_sat.py
@@ -5,18 +5,28 @@ Created on Tue May 19 12:22:07 2015
 @author: ibuder
 """
 
+import numpy as np
 import pandas as pd
 
+
+def make_float(d):
+    try:
+        return np.float(d)
+    except ValueError:
+        return np.NaN
 
 #  sat1314.xls has data on the second sheet (first is a title page)
 #  first column is the school code (CDS)
 sat = pd.io.excel.read_excel('sat1314.xls', sheetname=1, index_col=0)
+sat = sat[sat.RTYPE == u'S']  # only use schools (not districts/counties)
+sat['AVGSCR'] = (sat.AVGSCRMATH.map(make_float) +
+                    sat.AVGSCRREAD.map(make_float) +
+                    sat.AVGSCRWRIT.map(make_float))
 
 #  pubschls.xls first column is the school code (CDS)
 schools = pd.io.excel.read_excel('pubschls.xls', index_col=0)
 
-sat_schools = pd.merge(sat[['RTYPE', 'NUMTSTTAKR', 'AVGSCRREAD',
-                            'AVGSCRMATH', 'AVGSCRWRIT']],
+sat_schools = pd.merge(sat[['NUMTSTTAKR', 'AVGSCR']],
                             schools[['School', 'Street', 'City',
                             'Zip', 'State', 'Latitude', 'Longitude']],
                             how='inner',
